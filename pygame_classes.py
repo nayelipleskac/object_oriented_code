b15ee003c34df193d30ca708bc7cf7577667de19
Nayeli Pleskac <pleskac510@gmail.com>
2021-06-12 17:17:01 -0700

    circle race

--- pygame_classes.py
+++ pygame_classes.py
@@ -1,10 +1,10 @@
-from pygame.constants import KEYDOWN, QUIT
+from pygame.constants import KEYDOWN, K_SPACE, QUIT
 
 import pygame, random
 
 #1
 pygame.init() 
-screen = pygame.display.set_mode((600,600))
+screen = pygame.display.set_mode((600,700))
 pygame.display.set_caption("test")
 
 clock = pygame.time.Clock()
@@ -61,93 +61,149 @@ colors = [red, blue, green, purple, white, pink, orange, gray, yellow, aqua, lim
 #     circle10.draw_shapes()
 
 #2
-def showtext(msg,x,y,color):
-    font = pygame.font.SysFont("freesans", 32)
-    msgobj = font.render(msg,False,white)
-    screen.blit(msgobj,(x,y))
+# def showtext(msg,x,y,color):
+#     font = pygame.font.SysFont("freesans", 32)
+#     msgobj = font.render(msg,False,white)
+#     screen.blit(msgobj,(x,y))
 
 
-class circle:
+# class circle:
     
-    def __init__(self, x, y, ):
-        self.x = x
-        self.y = y
-        self.xMotion = random.randint(1,4)
-        self.color = random.choice(colors)
-        self.key =  random.choice(keys)
-        self.flag = False
-        self.timesPressed = 0
+#     def __init__(self, x, y, ):
+#         self.x = x
+#         self.y = y
+#         self.xMotion = random.randint(1,4)
+#         self.color = random.choice(colors)
+#         self.key =  random.choice(keys)
+#         self.flag = False
+#         self.timesPressed = 0
         
-    def draw_shapes(self):
-        if self.flag == False:
-            pygame.draw.circle(screen, self.color, (self.x, self.y), 25, 1)
-            showtext('{}'.format(self.key), self.x-10, self.y-15, white)
-        if self.flag == True:
-            pygame.draw.circle(screen, black, (self.x, self.y), 25, 1)
+#     def draw_shapes(self):
+#         if self.flag == False:
+#             pygame.draw.circle(screen, self.color, (self.x, self.y), 25, 1)
+#             showtext('{}'.format(self.key), self.x-10, self.y-15, white)
+#         if self.flag == True:
+#             pygame.draw.circle(screen, black, (self.x, self.y), 25, 1)
 
             
 
-            # if self.timesPressed == 2:
-            #     self.flag = False
+#             # if self.timesPressed == 2:
+#             #     self.flag = False
        
-    def defMove(self):
+#     def defMove(self):
+#         self.x += self.xMotion
+#         if self.x+25 >= 600:
+#             self.xMotion = -random.randint(1,4)
+#         if self.x-25 <= 0:
+#             self.xMotion = random.randint(1,4)
+
+# keys = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+# keyChoice = random.choice(keys)
+
+# circle1 = circle(random.randint(0,635), 30)
+# circle2 = circle(random.randint(0,635), 30)
+# circle3 = circle(random.randint(0,635), 30)
+# circle4 = circle(random.randint(0,635), 30)
+# circle5 = circle(random.randint(0,635), 30)
+# circle6 = circle(random.randint(0,635), 30)
+# circle7 = circle(random.randint(0,635), 30)
+
+# circle8 = circle(random.randint(25,635), 80)
+# circle9 = circle(random.randint(25,635), 80)
+# circle10 = circle(random.randint(25,635), 80)
+# circle11 = circle(random.randint(25,635), 80)
+
+
+# topCircles = [circle1, circle2, circle3, circle4, circle5, circle6, circle7]
+# bottomCircles = [circle8, circle9, circle10, circle11]
+# all = [circle1, circle2, circle3, circle4, circle5, circle6, circle7, circle8, circle9, circle10, circle11]
+
+
+
+# while True:
+#     clock.tick(30)
+#     pygame.display.update()
+#     screen.fill(black)
+#     for each in topCircles:
+#         # print(circle.xMotion)
+#         each.defMove()
+#         each.draw_shapes()
+#     for each in bottomCircles:
+#         # print(each.xMotion)
+#         each.defMove()
+#         each.draw_shapes()
+
+#     for event in pygame.event.get():
+#         if event.type == QUIT:
+#             pygame.quit()
+#             exit()
+#         elif event.type == KEYDOWN:
+#             print(event.key)
+#             for each in all:
+#                 if chr(event.key) == each.key:
+#                     each.flag = True
+#                     print(each.key,'we got the key!')
+#                     each.timesPressed = each.timesPressed+1
+#                     print(each.key, each.timesPressed)
+#                     if each.timesPressed % 2 == 0:
+#                         each.flag = False
+#                     # evenNum = each.timesPressed % 2
+#                     # if each.timesPressed == 2:
+#                     #     each.flag = False
+
+
+class circle:
+    def __init__(self, y):
+        self.x = 40
+        self.y = y
+        self.xMotion = random.randint(1,4)
+        self.color = random.choice(colors) 
+    def draw_shapes(self):
+        pygame.draw.circle(screen, self.color, (self.x, self.y), 25, 1)
+    def move_shapes(self):
         self.x += self.xMotion
         if self.x+25 >= 600:
             self.xMotion = -random.randint(1,4)
         if self.x-25 <= 0:
             self.xMotion = random.randint(1,4)
 
-keys = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-keyChoice = random.choice(keys)
 
-circle1 = circle(random.randint(0,635), 30)
-circle2 = circle(random.randint(0,635), 30)
-circle3 = circle(random.randint(0,635), 30)
-circle4 = circle(random.randint(0,635), 30)
-circle5 = circle(random.randint(0,635), 30)
-circle6 = circle(random.randint(0,635), 30)
-circle7 = circle(random.randint(0,635), 30)
+circle1 = circle(40)
+circle2 = circle(110)
+circle3 = circle(180)
+circle4 = circle(250)
+circle5 = circle(320)
+circle6 = circle(390)
+circle7 = circle(460)
+circle8 = circle(530)
+circle9 = circle(600)
+circle10 = circle(660)
 
-circle8 = circle(random.randint(25,635), 80)
-circle9 = circle(random.randint(25,635), 80)
-circle10 = circle(random.randint(25,635), 80)
-circle11 = circle(random.randint(25,635), 80)
-
-
-topCircles = [circle1, circle2, circle3, circle4, circle5, circle6, circle7]
-bottomCircles = [circle8, circle9, circle10, circle11]
-all = [circle1, circle2, circle3, circle4, circle5, circle6, circle7, circle8, circle9, circle10, circle11]
-
-
+all = [circle1, circle2, circle3, circle4, circle5, circle6, circle7, circle8, circle9, circle10]
 
 while True:
     clock.tick(30)
     pygame.display.update()
     screen.fill(black)
-    for each in topCircles:
-        # print(circle.xMotion)
-        each.defMove()
-        each.draw_shapes()
-    for each in bottomCircles:
-        # print(each.xMotion)
-        each.defMove()
-        each.draw_shapes()
 
+    for each in all:
+        each.draw_shapes()
+        each.move_shapes()
     for event in pygame.event.get():
+    
         if event.type == QUIT:
             pygame.quit()
             exit()
-        elif event.type == KEYDOWN:
-            print(event.key)
-            for each in all:
-                if chr(event.key) == each.key:
-                    each.flag = True
-                    print(each.key,'we got the key!')
-                    each.timesPressed = each.timesPressed+1
-                    print(each.key, each.timesPressed)
-                    evenNum = each.timesPressed % 2
-                    if each.timesPressed == 2:
-                        each.flag = False
+        #starting with the space bar
+        # elif event.type == KEYDOWN:
+        #     if event.key == K_SPACE:
+        #         for each in all:
+        #             each.move_shapes()
+
+                
+
+
+
 
                     
 
