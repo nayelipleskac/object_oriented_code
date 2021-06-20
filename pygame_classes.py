0117d97e3f2bc80a3d30e24ab727101e4aff3f0b
Nayeli Pleskac <pleskac510@gmail.com>
2021-06-19 21:57:00 -0700

    class vars

--- pygame_classes.py
+++ pygame_classes.py
@@ -1,4 +1,4 @@
-from pygame.constants import KEYDOWN, K_SPACE, QUIT
+from pygame.constants import KEYDOWN, K_DOWN, K_LEFT, K_RIGHT, K_SPACE, K_UP, MOUSEBUTTONDOWN, QUIT
 
 import pygame, random
 
@@ -152,105 +152,270 @@ colors = [red, blue, green, purple, white, pink, orange, gray, yellow, aqua, lim
 #                     #     each.flag = False
 
 
-def showtext(msg,x,y,color):
-    font = pygame.font.SysFont("freesans", 32)
-    msgobj = font.render(msg,False,white)
-    screen.blit(msgobj,(x,y))
+# def showtext(msg,x,y,color):
+#     font = pygame.font.SysFont("freesans", 32)
+#     msgobj = font.render(msg,False,white)
+#     screen.blit(msgobj,(x,y))
 
-placement_finisher_list = []
+# placement_finisher_list = []
 
-class Circle:
+
+
+# class Circle:
+#     global finishers
+#     finishers = 0
+#     def __init__(self, y):
+#         self.x = 40
+#         self.y = y
+#         self.xMotion = random.randint(1,4)
+#         self.color = random.choice(colors) 
+#         self.lap_count = 0
+        
+#     def draw_shapes(self):
+#         # for each in circles:
+#         #         placement_finisher_list.append(self.placement_count)
+#         #         for eachC in placement_finisher_list:
+#         #             max(placement_finisher_list) +1
     
-    def __init__(self, y):
-        self.x = 40
-        self.y = y
-        self.xMotion = random.randint(1,4)
-        self.color = random.choice(colors) 
-        self.lap_count = 0
-        self.finishers = 0
-        # self.finisherList = []
-        self.placement_count = 0
-    def draw_shapes(self):
-        # for each in circles:
-        #         placement_finisher_list.append(self.placement_count)
-        #         for eachC in placement_finisher_list:
-        #             max(placement_finisher_list) +1
-        if self.lap_count == 2:
-            #the lap count and the placement count are the same so when self.lapcount == 2 then all the circs are turning blue bc that waht the placement count is 
-            self.finishers += 1
+#         pygame.draw.circle(screen, self.color, (self.x, self.y), 25, 1)
+
+#     def checkCircles(self, finishers):
+
+#         if self.lap_count >= 2:
+#             finishers += 1
+#             print('self.lap_count is >= to 2')
             
-            if self.finishers == 1:
-            # if self.placement_count == 1:
-                self.color = red
-                self.x = 40
-            if self.finishers == 2:
-            # if self.placement_count == 2:
-                self.color = blue
-                self.x = 40
-            if self.finishers == 3:
-            # if self.placement_count == 3:
-                self.color = green
-                self.x = 40 
-            if self.finishers >= 4:
-            # if self.placement_count >= 4:
-                self.x = 40
-                self.color = purple
-
-        pygame.draw.circle(screen, self.color, (self.x, self.y), 25, 1)
-    
+#             if finishers == 1:
             
-    def move_shapes(self):
-        self.x += self.xMotion
-        if self.x+25 >= 600:
-            self.xMotion = -random.randint(1,4)
-        if self.x-25 <= 0:
-            self.xMotion = random.randint(1,4)
+#                 self.color = red
+#                 self.x = 40
+#             if finishers == 2:
+            
+#                 self.color = blue
+#                 self.x = 40
+#             if finishers == 3:
+            
+#                 self.color = green
+#                 self.x = 40 
+#             if finishers >= 4:
 
-        if self.finishers >= 1:
-            showtext('WE HAVE A WINNER!!', 160, 40, white)
+#                 self.x = 40
+#                 self.color = purple
 
-circle1 = Circle(40)
-circle2 = Circle(110)
-circle3 = Circle(180)
-circle4 = Circle(250)
-circle5 = Circle(320)
-circle6 = Circle(390)
-circle7 = Circle(460)
-circle8 = Circle(530)
-circle9 = Circle(600)
-circle10 = Circle(660)
+#     def move_shapes(self):
+#         self.x += self.xMotion
+#         if self.x+25 >= 600:
+#             self.xMotion = -random.randint(1,4)
+#         if self.x-25 <= 0:
+#             self.xMotion = random.randint(1,4)
 
-circles = [circle1, circle2, circle3, circle4, circle5, circle6, circle7, circle8, circle9, circle10]
-# some = [circle1, circle2, circle3]
+#         if finishers >= 1:
+#             showtext('WE HAVE A WINNER!!', 160, 40, white)
+
+# circle1 = Circle(40)
+# circle2 = Circle(110)
+# circle3 = Circle(180)
+# circle4 = Circle(250)
+# circle5 = Circle(320)
+# circle6 = Circle(390)
+# circle7 = Circle(460)
+# circle8 = Circle(530)
+# circle9 = Circle(600)
+# circle10 = Circle(660)
+
+# circles = [circle1, circle2, circle3, circle4, circle5, circle6, circle7, circle8, circle9, circle10]
 
 
-while True:
-    clock.tick(30)
-    pygame.display.update()
-    screen.fill(black)
+# while True:
+#     clock.tick(30)
+#     pygame.display.update()
+#     screen.fill(black)
 
-    for c in circles:
-        c.draw_shapes()
-        c.move_shapes()
+#     for c in circles:
+#         c.draw_shapes()
+#         c.move_shapes()
+#         c.checkCircles(finishers)
 
-    for event in pygame.event.get():
-        if event.type == QUIT:
-                pygame.quit()
-                exit()
-    for c in circles:
-        if c.x-25 == 0:
-            c.lap_count+=1
-            # c.placement_count+=1
-            print(c.finishers)
-            # print(c.placement_count)
-            if c.placement_count != 0:
-                #lock in?
-                c.placement_count = c.placement_count
+
+#     for event in pygame.event.get():
+#         if event.type == QUIT:
+#                 pygame.quit()
+#                 exit()
+
+#     for c in circles:
+#         if c.x-25 == 0:
+#             c.lap_count+=1
+            
+            # if c.lap_count >= 2:
+            #     finishers += 1
+            # if c.lap_count == 0:
+            #     print(finishers)
+            #     print('lap_count = 0')
+            #     print(finishers)
+            # if c.lap_count == 1:
+            #     print('lap_count = 0')
+            #     print(finishers)
+            # if c.lap_count == 2:
+            #     print(finishers)
+            
+            
 
         # elif event.type == KEYDOWN:
         #         if event.key == K_SPACE:
         #             for each in all:
         #                 each.move_shapes()
+
+#test
+# class Circle:
+#     radius = 15
+#     def __init__(self):
+#         pass
+#     def draw(self):
+#         pygame.draw.circle(screen, blue, (self.x, self.y), 25, 1)
+
+# ob1 = Circle()
+# ob2 = Circle()
+# #changes the radius for all objects
+# Circle.radius = 20
+# #changes the radius for one object
+# ob1.radius = 17
+# print(ob1.radius)
+# print(Circle.radius)
+# print(ob2.radius)
+
+
+class Circle:
+    radius = 20
+    def __init__(self):
+        self.x = random.randint(10,590)
+        self.y = random.randint(10, 690)
+        self.color = random.choice(colors)
+    def draw(self):
+        pygame.draw.circle(screen, self.color, (self.x, self.y), self.radius, 1)
+
+circle1 = Circle()
+circle2 = Circle()
+circle3 = Circle()
+circle4 = Circle()
+circle5 = Circle()
+circle6 = Circle()
+circle7 = Circle()
+circle8 = Circle()
+circle9 = Circle()
+circle10 = Circle()
+
+circles = [circle1, circle2, circle3, circle4, circle5, circle6, circle7, circle8, circle9, circle10]
+
+
+while True:
+    screen.fill(black)
+    for c in circles:
+        c.draw()
+    clock.tick(50)
+    pygame.display.update()
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            exit()
+        elif event.type == KEYDOWN:
+            if event.key == K_UP:
+                Circle.radius = Circle.radius + 2
+            if event.key == K_DOWN:
+                Circle.radius = Circle.radius - 2 
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            print(event.button)
+            if event.button == K_RIGHT:
+                Circle.radius = Circle.radius + 2
+                pass
+            if event.button == K_LEFT:
+                Circle.radius = Circle.radius -2
+                pass
+                
+
+      
+#Circle Race Game
+# import pygame, random
+# pygame.init()
+# screen = pygame.display.set_mode((1000, 600))
+# clock = pygame.time.Clock()
+# class Circle():
+#     def __init__(self, color, x, y, radius, speed, lap):
+#         self.color = color
+#         self.x = x
+#         self.y = y
+#         self.radius = radius
+#         self.speed = speed
+#         self.lap = lap
+#     def draw(self):
+#         pygame.draw.circle(screen, self.color, (self.x, self.y), self.radius)
+# # colors
+# black = (0, 0, 0)
+# red = (255, 0, 0)
+# green = (0, 255, 0)
+# blue = (0, 0, 255)
+# white = (255, 255, 255)
+# yellow = (200, 200, 0)
+# # circles
+# circle = []
+# x = 50
+# radius = 25
+# lap = 0
+# for y in range(50, 550, 50):
+#     speed = random.randint(1, 5)
+#     c = Circle(white, x, y, radius, speed, lap)
+#     circle.append(c)
+# # positions
+# first = 1
+# second = 0
+# third = 0
+# while True:
+#     # fill
+#     screen.fill(black)
+#     # close event
+#     for event in pygame.event.get():
+#         if event.type == pygame.QUIT:
+#             pygame.quit()
+#             exit()
+#     # draw circle
+#     for c in circle:
+#         c.draw()
+#         c.x = c.x + c.speed
+#         # bounce
+#         if c.x > 950 or c.x < 25:
+#             c.speed = -c.speed
+#         # lap count
+#         if c.x <= 25:
+#             c.lap = c.lap + 1
+#             print('circle', circle.index(c), 'lap', c.lap)
+#         # color change
+#         if c.lap >= 5 and first == 1:
+#             first = 0
+#             second = 1
+#             c.color = red
+#             c.x = 26
+#             c.speed = 0
+#             c.lap=-1
+#         elif c.lap >= 5 and second == 1:
+#             first = 0
+#             second = 0
+#             third = 1
+#             c.color = green
+#             c.x = 26
+#             c.speed = 0
+#             c.lap=-1
+#         elif c.lap >= 5 and third == 1:
+#             first = 0
+#             second = 0
+#             third = 0
+#             c.color = blue
+#             c.x = 26
+#             c.speed = 0
+#             c.lap=-1
+#     # clock
+#     clock.tick(100)
+#     # update
+#     pygame.display.update()
+
 
 
                 
