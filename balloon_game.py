31d8e4fc087470bdfc42f5767081c4303eee937d
Nayeli Pleskac <pleskac510@gmail.com>
2021-07-14 15:31:22 -0700

    balloon sprite

--- balloon_game.py
+++ balloon_game.py
@@ -24,7 +24,7 @@ def showtext(msg,x,y,color):
     screen.blit(msgobj,(x,y))
 
 
-class circle:
+class balloon:
     
     def __init__(self, x, y):
         self.x = x
@@ -34,6 +34,7 @@ class circle:
         self.key =  random.choice(keys)
         self.flag = False
         self.timesPressed = 0
+        self.image = pygame.image.load('')
         
     def draw_shapes(self):
         if self.flag == False:
@@ -41,11 +42,6 @@ class circle:
             showtext('{}'.format(self.key), self.x-10, self.y-15, white)
         if self.flag == True:
             pygame.draw.circle(screen, black, (self.x, self.y), 25, 1)
-
-            
-
-            # if self.timesPressed == 2:
-            #     self.flag = False
        
     def move(self):
         self.x += self.xMotion
@@ -57,17 +53,17 @@ class circle:
 keys = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 keyChoice = random.choice(keys)
 
-circle1 = circle(random.randint(0,635), random.randint(25,575))
-circle2 = circle(random.randint(0,635), random.randint(25,575))
-circle3 = circle(random.randint(0,635), random.randint(25,575))
-circle4 = circle(random.randint(0,635), random.randint(25,575))
-circle5 = circle(random.randint(0,635), random.randint(25,575))
-circle6 = circle(random.randint(0,635), random.randint(25,575))
-circle7 = circle(random.randint(0,635), random.randint(25,575))
+circle1 = balloon(random.randint(0,635), random.randint(25,575))
+circle2 = balloon(random.randint(0,635), random.randint(25,575))
+circle3 = balloon(random.randint(0,635), random.randint(25,575))
+circle4 = balloon(random.randint(0,635), random.randint(25,575))
+circle5 = balloon(random.randint(0,635), random.randint(25,575))
+circle6 = balloon(random.randint(0,635), random.randint(25,575))
+circle7 = balloon(random.randint(0,635), random.randint(25,575))
 
-circle8 = circle(random.randint(25,635), random.randint(25,575))
-circle9 = circle(random.randint(25,635), random.randint(25,575))
-circle10 = circle(random.randint(25,635), random.randint(25,575))
+circle8 = balloon(random.randint(25,635), random.randint(25,575))
+circle9 = balloon(random.randint(25,635), random.randint(25,575))
+circle10 = balloon(random.randint(25,635), random.randint(25,575))
 
 circles_list = [circle1, circle2, circle3, circle4, circle5, circle6, circle7, circle8, circle9, circle10]
 
@@ -85,12 +81,12 @@ while True:
             exit()
         elif event.type == KEYDOWN:
             print(event.key)
-            for c in circles_list:
-                if chr(event.key) == c.key:
-                    c.flag = True
-                    print(c.key,'we got the key!')
-                    c.timesPressed = c.timesPressed+1
-                    print(c.key, c.timesPressed)
-                    if c.timesPressed % 2 == 0:
-                        c.flag = False
+            # for c in circles_list:
+            if chr(event.key) == c.key:
+                c.flag = True
+                print(c.key,'we got the key!')
+                c.timesPressed = c.timesPressed+1
+                print(c.key, c.timesPressed)
+                if c.timesPressed % 2 == 0:
+                    c.flag = False
              
